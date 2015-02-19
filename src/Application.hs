f99d4a17052d5fe3bbb25dd4b522e871de0b0707
Will Langstroth <will@langstroth.com>
2015-02-18 20:22:51 -0500

    Add data handling for flower list

--- src/Application.hs
+++ src/Application.hs
@@ -1,25 +1,33 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 ------------------------------------------------------------------------------
 -- | This module defines our application's state type and an alias for its
 -- handler monad.
 module Application where
 
-import Control.Lens
-import Snap.Snaplet
-import Snap.Snaplet.Heist
-import Snap.Snaplet.Auth
-import Snap.Snaplet.Session
+import           Control.Lens
+import           Snap.Snaplet
+import           Snap.Snaplet.Heist
+import           Snap.Snaplet.Auth
+import           Snap.Snaplet.AcidState
+import           Snap.Snaplet.Session
+
+import           Types
 
 data App = App
     { _heist :: Snaplet (Heist App)
     , _sess :: Snaplet SessionManager
     , _auth :: Snaplet (AuthManager App)
+    , _flowers :: Snaplet (Acid FlowerDB)
     }
 
 makeLenses ''App
 
 instance HasHeist App where
     heistLens = subSnaplet heist
+
+instance HasAcid App FlowerDB where
+    getAcidStore = view (flowers . snapletValue)
 
 type AppHandler = Handler App App
--- src/Site.hs
+++ src/Site.hs
@@ -9,7 +9,11 @@ module Site
   ) where
 
 import           Data.ByteString (ByteString)
+import qualified Data.Map as Map
+
+import           Snap.Core
 import           Snap.Snaplet
+import           Snap.Snaplet.AcidState
 import           Snap.Snaplet.Auth
 import           Snap.Snaplet.Auth.Backends.JsonFile
 import           Snap.Snaplet.Heist
@@ -17,22 +21,24 @@ import           Snap.Snaplet.Session.Backends.CookieSession
 import           Snap.Util.FileServe
 
 import           Application
+import           Types
+import           Handlers
 
 routes :: [(ByteString, Handler App App ())]
-routes = [ ("", serveDirectory "static")
+routes = [ ("", ifTop homePage)
+         , ("", serveDirectory "static")
+         , ("/admin/flowers", adminFlowerListPage)
          ]
 
 app :: SnapletInit App App
 app = makeSnaplet "app" "An snaplet example application." Nothing $ do
+    addRoutes routes
     h <- nestSnaplet "" heist $ heistInit "templates"
     s <- nestSnaplet "sess" sess $
            initCookieSessionManager "site_key.txt" "sess" (Just 3600)
-
-    -- NOTE: We're using initJsonFileAuthManager here because it's easy and
-    -- doesn't require any kind of database server to run.  In practice,
-    -- you'll probably want to change this to a more robust auth backend.
     a <- nestSnaplet "auth" auth $
            initJsonFileAuthManager defAuthSettings sess "users.json"
+    d <- nestSnaplet "flowers" flowers $ acidInit (FlowerDB Map.empty)
     addRoutes routes
     addAuthSplices h auth
-    return $ App h s a
+    return $ App h s a d
