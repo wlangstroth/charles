952456e2d4734fab3181d83fcee58063e8c566a6
Will Langstroth <will@langstroth.com>
2015-05-01 10:54:53 -0400

    Clean up handlers

--- src/Handlers.hs
+++ src/Handlers.hs
@@ -8,14 +8,13 @@ import           Prelude hiding (div)
 import           Control.Monad.Trans (lift)
 
 import qualified Data.Text as T
--- import qualified Data.Text.Encoding as E
+import           Data.Text (Text)
 
 import           Text.Blaze
 import           Text.Blaze.Html5 hiding (map)
 import qualified Text.Blaze.Html5.Attributes as A
 import           Text.Blaze.Renderer.XmlHtml
 
-import           Text.Digestive as D
 import           Text.Digestive.Heist ()
 import           Text.Digestive.Snap ()
 
@@ -37,9 +36,6 @@ homePage :: AppHandler ()
 homePage = Core.method GET $ renderWithSplices "index" $
             "flowers" ## flowerListSplice
 
-adminHomePage :: AppHandler ()
-adminHomePage = Core.method GET $ render "admin-index"
-
 flowerListSplice :: I.Splice AppHandler
 flowerListSplice = do
     theFlowers <- lift flowerList
@@ -55,61 +51,31 @@ flowerRow (FlowerListing _ latin common desc price bloom) =
             td (toHtml price)
             td (toHtml bloom)
 
-adminFlowerListSplice :: I.Splice AppHandler
-adminFlowerListSplice = do
-    theFlowers <- lift flowerList
-    return $ concatMap adminFlowerRow theFlowers
-
-adminFlowerRow :: FlowerListing -> [X.Node]
-adminFlowerRow (FlowerListing _ latin common desc price bloom) =
-    renderHtmlNodes $
-        tr $ do
-            td $
-                a ! A.href flowerLink $ toHtml latin
-            td (toHtml common)
-            td (toHtml desc)
-            td (toHtml price)
-            td (toHtml bloom)
-  where
-    flowerLink = toValue $ T.concat ["/admin/flowers/", latin]
-
-adminFlowerListPage :: AppHandler ()
-adminFlowerListPage =  renderWithSplices "admin-flower-list" $
-                            "flowers" ## adminFlowerListSplice
-
 flowerListPage :: AppHandler ()
 flowerListPage =  renderWithSplices "flowers" $ "flowers" ## flowerListSplice
 
-    -- (view, result) <- runForm "flower" newFlowerForm
-    -- case result of
-    --    Just flowerItem -> do
-    --        _ <- update $ FlowerInsert flowerItem
-    --        heistLocal (bindDigestiveSplices view) $
-    --            renderWithSplices "admin-flower-list" $
-    --                "flowers" ## adminFlowerListSplice
-    --    Nothing -> heistLocal (bindDigestiveSplices view) $
-    --               renderWithSplices "admin-flower-list" $
-    --                   "flowers" ## adminFlowerListSplice
+galleryPage :: AppHandler ()
+galleryPage = renderWithSplices "gallery" $ "gallery" ## imageGallerySplice
 
-adminFlowerForm :: Monad m => Maybe FlowerListing -> Form T.Text m FlowerListing
-adminFlowerForm flower = FlowerListing
-    <$> "id" .: D.stringRead "Not a number" (fmap flowerId flower)
-    <*> "latin" .: D.text (fmap flowerLatinName flower)
-    <*> "common" .: D.text (fmap flowerCommonNames flower)
-    <*> "description" .: D.text (fmap flowerDescription flower)
-    <*> "price" .: D.text (fmap flowerPriceDescription flower)
-    <*> "bloom" .: D.text (fmap flowerBloomRange flower)
+imageGallerySplice :: I.Splice AppHandler
+imageGallerySplice = do
+    images <- lift galleryList
+    return $ concatMap galleryImageBox images
 
-newFlowerForm :: Monad m => Form T.Text m FlowerListing
-newFlowerForm = FlowerListing
-    <$> "id" .:  D.stringRead "Not a number" Nothing
-    <*> "latin" .: D.text Nothing
-    <*> "common" .: D.text Nothing
-    <*> "description" .: D.text Nothing
-    <*> "price" .: D.text Nothing
-    <*> "bloom" .: D.text Nothing
+imageUrl :: GalleryImage -> Text
+imageUrl (GalleryImage im s _) = T.concat [sourcePrefix s, im, ".jpg"]
+  where
+    sourcePrefix Twitter = "https://pbs.twimg.com/media/"
+    sourcePrefix OtherSource = "http://thing.org/"
 
-handleLogin :: Maybe T.Text -> Handler App (AuthManager App) ()
+galleryImageBox :: GalleryImage -> [X.Node]
+galleryImageBox image = renderHtmlNodes $
+        div $
+            img ! A.src (toValue $ imageUrl image)
+
+-- User management ------------------------------------------------------------
+
+handleLogin :: Maybe Text -> Handler App (AuthManager App) ()
 handleLogin authError = heistLocal (I.bindSplices errs) $ render "login"
   where
     errs = maybe noSplices splice authError
